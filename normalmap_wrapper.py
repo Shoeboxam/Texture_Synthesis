694f105d690398d8af10dfb807876d35b8d7df1c
Shoeboxam <shoeboxam@gmail.com>
2014-12-16 18:13:43 -0600

    Just use normal map plugin... it works

--- normalmap_wrapper.py
+++ normalmap_wrapper.py
@@ -13,14 +13,12 @@ def modify_path(image, insertion):
   return filename[:index_insertion] + insertion + filename[index_insertion:]
  
 # This does all the work
-def normalmap_wrapper(glob_pattern, source, depth):
-
-  path_suffixes = [ "_n", "_s", "_d", "_a", "_h", "_hn", "_ln", "_mn", "_sn" ]
-
-
+def normalmap_wrapper(glob_pattern, source):
+  # Send errors and log to file
   # Send errors and log to file
   sys.stderr = open(os.path.expanduser('~\Desktop\errorstream.txt'), 'a')
   sys.stdout = open(os.path.expanduser('~\Desktop\outstream.txt'), 'a')
+
   # Get all the files from the directory
   #glob1 = glob.glob(source + glob_pattern)
   files = []
@@ -35,32 +33,9 @@ def normalmap_wrapper(glob_pattern, source, depth):
     image = pdb.gimp_file_load(path, path)
     drawable = pdb.gimp_image_get_active_layer(image)
 
-    resize_factor = 3
 
-    # Tile to fix interpolation of edge pixels
-    pdb.plug_in_tile(image, drawable, image.width * resize_factor, image.height * resize_factor, False)
-
-    # Vertical flip to invert light cast
-    image = pdb.gimp_image_flip(image, 1)
-
-    #(image, drawable, remove_lighting, resizie, tile, new_width, edge_specular, def_specular, 
-      #depth, large_details, medium_details, small_details, shape_recog, smoothstep, noise, invh, ao, prev)
-    pdb.plug_in_insanebump(image, drawable, True, False, False, 1, 0, 255, depth, 0, 0, 100, 0, False, False, False, 0, False)
-
-    for suffix in path_suffixes[1:]:
-      os.remove(modify_path(suffix))
-
-    map_normal_layer = pdb.gimp_file_load_layer(image, modify_path(path_suffixes[0]))
-
-    # Unflip!
-    image = pdb.gimp_image_flip(image, 1)
-
-    # Untile!
-    pdb.gimp_image_crop(image, image.width / resize_factor, image.height / resize_factor, (image.width - (image.width / resize_factor)) / 2, (image.height - (image.height / resize_factor)) / 2)
-
-
-    # Alternative normal script plugin
-    #pdb.plug_in_normalmap(image, drawable, 0, .04, 6, 1, 0, 1, 0, 0, 0, 1, 0, 0, drawable)
+    # Call the normal script plugin
+    pdb.plug_in_normalmap(image, drawable, 0, .04, 6, 1, 0, 1, 0, 0, 0, 1, 0, 0, drawable)
 
 
     # Change to new file name
@@ -80,8 +55,7 @@ register(
   "",
   [
     (PF_STRING, "glob_pattern", "Glob Pattern", "*.png"),
-    (PF_STRING, "source", "Source Directory", ""),
-    (PF_INT, "depth", "Depth", 3)
+    (PF_STRING, "source", "Source Directory", "")
   ],
   [],
   normalmap_wrapper,
