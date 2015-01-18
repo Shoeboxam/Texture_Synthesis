328e30d56aaa9ad54e37aa77ba85468d34d8ec00
Shoeboxam <shoeboxam@gmail.com>
2015-01-18 17:12:39 -0600

    Recognition code cleanup
    I'm getting an error partway through runtime from scipy stats, I'm going to have to investigate it...

--- Pillow/find_match.py
+++ Pillow/find_match.py
@@ -1,23 +1,12 @@
-import numpy as np
 from scipy.stats.stats import pearsonr
 import os
-from PIL import ImageEnhance
 from PIL import Image
 
 home = r"C:\Users\mike_000\AppData\Roaming\.ftb\FTBResurrection\minecraft"
-threshold = 0.90
+threshold = 0.80
 
-template_img = ImageEnhance.Color(Image.open(r"C:\Users\mike_000\Desktop\planks_oak.png")).enhance(0)
+template_img = Image.open(r"C:\Users\mike_000\Desktop\planks_oak.png")
 template = template_img.convert("L").getdata()
-
-
-def correlate(list_one, list_two, threshold):
-    try:
-        correlation = pearsonr(list_one, list_two)[0]
-        # print(correlation)
-        return correlation > threshold
-    except:
-        return False
 
 for root, dirs, files in os.walk(home + "\\resourcepacks\\default\\"):
     for current_file in files:
@@ -26,7 +15,5 @@ for root, dirs, files in os.walk(home + "\\resourcepacks\\default\\"):
         if (img.size == template_img.size):
             candidate = img.convert("L").getdata()
 
-            if(correlate(template, candidate, threshold)):
+            if(pearsonr(template, candidate)[0] > threshold):
                 print(current_file)
-
-            # np.asarray()
