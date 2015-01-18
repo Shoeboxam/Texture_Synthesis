19fdb8b3b20846e99d70682f6dc4d6f3aa7368cc
Shoeboxam <shoeboxam@gmail.com>
2015-01-18 01:36:23 -0600

    Cleanup empty folders in make default

--- make_default.py
+++ make_default.py
@@ -15,3 +15,19 @@ for root, dirs, files in os.walk(home + "\\resourcepacks\\default\\"):
     for currentFile in files:
         if not currentFile.endswith('.png'):
             os.remove(os.path.join(root, currentFile))
+
+
+def cleanup_folder(path):
+    file_list = os.listdir(path)
+
+    # Recurse if child folder
+    for file in file_list:
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            cleanup_folder(file_path)
+
+    # Delete if empty folder
+    if len(file_list) == 0:
+        os.rmdir(path)
+
+cleanup_folder(home + "\\resourcepacks\\default\\")
