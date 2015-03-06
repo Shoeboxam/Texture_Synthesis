598c1bf33709e9695ab273e36df7355f1075272e
Shoeboxam <shoeboxam@gmail.com>
2015-03-05 22:05:37 -0600

    make_default.py generates mod files in git repository format

--- make_default.py
+++ make_default.py
@@ -1,33 +1,80 @@
 import zipfile
 import os
+from distutils.dir_util import copy_tree
+import shutil
+
+key_repository = 'C:\\Users\mike_000\Textures\Modded-1.7.x'
+
+home = "C:\\Users\mike_000\AppData\Roaming\.ftb\FTBInfinity\minecraft"
 
 
-home = os.getcwd()
+def remove_cruft(target_path):
+    # Filter out all filetypes that are not .png
+    for root, folders, files in os.walk(target_path, topdown=False):
+        for current_file in files:
+            if not current_file.endswith('.png'):
+                try:
+                    os.remove(os.path.join(root, current_file))
+                except FileNotFoundError:
+                    print("Skipping deletion of " + os.path.join(root, current_file))
 
-for file in os.listdir(home + "\\mods\\"):
-    if file.endswith(".jar"):
-        zip_ob = zipfile.ZipFile(home + "\\mods\\" + file)
-        zip_ob.extractall(home + "\\resourcepacks\\default\\")
-
-        print(file)
-
-for root, dirs, files in os.walk(home + "\\resourcepacks\\default\\"):
-    for currentFile in files:
-        if not currentFile.endswith('.png'):
-            os.remove(os.path.join(root, currentFile))
+        for current_folder in folders:
+            folder_name = root + "\\" + current_folder
+            if not os.listdir(folder_name):
+                os.rmdir(folder_name)
 
 
-def cleanup_folder(path):
-    file_list = os.listdir(path)
+def make_default(home, separate=True):
+    for file in os.listdir(home + "\\mods\\"):
+        if file.endswith(".jar"):
+            try:
+                zip_ob = zipfile.ZipFile(home + "\\mods\\" + file)
 
-    # Recurse if child folder
-    for file in file_list:
-        file_path = os.path.join(path, file)
-        if os.path.isdir(file_path):
-            cleanup_folder(file_path)
+                mod_path = home + "\\resourcepacks\\temp\\"
+                if (separate):
+                    mod_path += ("\\" + file)
 
-    # Delete if empty folder
-    if len(file_list) == 0:
-        os.rmdir(path)
+                try:
+                    zip_ob.extractall(mod_path)
+                except PermissionError:
+                    print("Skipped " + file + " [PermissionError]")
+                except FileNotFoundError:
+                    pass
 
-cleanup_folder(home + "\\resourcepacks\\default\\")
+                try:
+                    os.rename(mod_path, home + "\\resourcepacks\\temp\\" + os.listdir(mod_path + "\\assets")[0])
+                except FileNotFoundError:
+                    print("Skipping rename of " + file + " [FileNotFoundError]")
+                    shutil.rmtree(mod_path)
+                except FileExistsError:
+                    copy_tree(mod_path, home + "\\resourcepacks\\temp\\" + os.listdir(mod_path + "\\assets")[0])
+                    shutil.rmtree(mod_path)
+
+                print(file)
+
+            finally:
+                # Remove lock
+                zip_ob.close()
+
+
+def extract_matches(source, output, key_repository):
+
+    asset_dictionary = {}
+    for folder in os.listdir(key_repository):
+        try:
+            asset_dictionary[folder] = os.listdir(key_repository + '//' + folder + '//assets' )[0]
+        except FileNotFoundError:
+            pass  # Ignore nonstandard and .git folders
+
+    for k, v in asset_dictionary.items():
+        if v in os.listdir(source):
+            copy_tree(source + "\\" + v, output + "\\" + k)
+
+
+shutil.rmtree(home + "\\resourcepacks\\temp\\", True)
+
+make_default(home)
+remove_cruft(home + "\\resourcepacks\\default\\")
+extract_matches(home + "\\resourcepacks\\temp\\", home + "\\resourcepacks\\default\\", key_repository)
+
+shutil.rmtree(home + "\\resourcepacks\\temp\\", True)
