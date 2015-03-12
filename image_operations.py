c054f25928be79b963f70f991548b5a3514b572d
Shoeboxam <shoeboxam@gmail.com>
2015-03-12 04:35:03 -0500

    Semi-functional image slicing

--- image_operations.py
+++ image_operations.py
@@ -1,10 +1,10 @@
 import os
 from PIL import Image
-import PIL.ImageOps
 import json
 import math
-import np
+import numpy as np
 
+np.set_printoptions(threshold=np.nan)
 
 def list_templates(template_path):
     """Returns a list of names of key-value template image pairs"""
@@ -21,27 +21,68 @@ def list_templates(template_path):
     return matches
 
 
-def template_decompose(template, layers):
+def image_decompose(source, layers):
+    """Slice image by a given number of lightness zones"""
 
-    master_mask = template.convert("L")
+    master_mask_image = source.convert("L")
+    width, height = source.size
 
-    opacity_range = 1 / layers
+    # Convert to numpy array, then normalize to range of opacity variables
+    master_mask = np.asarray(master_mask_image).astype(float).flatten() / 256
+    master_mask_range = master_mask.max() - master_mask.min()
+
+    # Create linearly-spaced value based layer masks spread over range of source image lightness
+    mask_list = []
+    opacity_range = master_mask_range / layers
+
     for current_layer in range(layers):
         pixels_mask = []
-        opacity_peak = opacity_range * current_layer - (opacity_range / 2)
-        for lightness in np.asarray(master_mask)[0]:
-            # If lightness is within first period
+
+        # Peak opacity = lowest opacity + spacers + 50% offset
+        opacity_peak = master_mask.min() + (opacity_range * current_layer) + (opacity_range / 2)
+
+        for lightness in master_mask:
+            # If lightness is within mask's opacity period...
             if lightness > opacity_peak - (opacity_range / 2) and lightness < opacity_peak + (opacity_range / 2):
-                # Then weight opacity sinusoidally
-                pixels_mask.append(math.cos(2 * math.pi * lightness - 2 * math.pi * opacity_peak) / 2 + .5)
+
+                # Resolve edge case to preserve transparency in low and high lightness
+                if lightness < master_mask.min() + opacity_range / 2 or lightness > master_mask.max() - opacity_range / 2:
+                    pixels_mask.append(1)
+                else:
+                    # ... then weight opacity sinusoidally- two adjacent layers have inverse opacities, therefore preserving original value
+                    pixels_mask.append(math.cos(2 * math.pi * lightness - 2 * math.pi * opacity_peak) / 2 + .5)
+
             else:
+                # Avoid opacity contamination from periodicity in cosine
                 pixels_mask.append(0)
 
-        width, height = template.size
+        # Reformat pixel list into rows and columns, then into image
+        pixels_mask = np.reshape(pixels_mask, (width, height))
 
-        Image.fromarray(np.reshape(pixels_mask, width, height))
+        mask_list.append(Image.fromarray(pixels_mask))
 
-        Image.alpha_composite()
+    # Apply all transparency masks to source image
+    layer_list = []
+    for mask in mask_list:
+        mask = np.asarray(mask).reshape(width * height)
+        source_pixels = np.asarray(source).reshape(width * height, 4)
+
+        source_pixels.flags.writeable = True
+
+        source_pixels[:, 3] = mask * 256
+        masked_source_pixels = np.reshape(source_pixels, (width, height, 4))
+        print(masked_source_pixels)
+
+        layer_list.append(Image.fromarray(masked_source_pixels, mode='RGBA'))
+
+    return layer_list
+
+
+def image_compose(layers):
+    canvas = layers[0]
+    for index in range(1, len(layers) - 1):
+        canvas = Image.alpha_composite(canvas, layers[index + 1])
+    return canvas
 
 
 def populate_images(templates_path, metadata_pack, output_path):
@@ -49,3 +90,9 @@ def populate_images(templates_path, metadata_pack, output_path):
     for root, folders, files in os.walk(metadata_pack):
         for file in files:
             full_path = os.path.join(root, file)
+
+
+image_list = image_decompose(Image.open(r"C:\Users\mike_000\Desktop\pack.png"), 2)
+
+for num, image in enumerate(image_list):
+    image.save(r"C:\Users\mike_000\Desktop\\" + str(num) + ".png")
