6b6bd385e2beb6502cda30805f63d120e0fe0894
Shoeboxam <shoeboxam@gmail.com>
2015-06-06 00:51:35 -0500

    Time to burn it down
    Ive redesigned everything above the image graph. Ive managed to remove all data and computational redundancy, leaving a generic prebaked config system. Resource pack computation should only take minutes (pre-baking will take several days to compute). My plan is to open a separate config file repository here on github. A side program will clone/read the configs and an input resource pack, and generate the output relatively quickly. Im, err, starting to write that now, while I wait for some kind of ok from the curse folks (I don't want to just scrape my web services provider) :D

--- Minecraft/web_utilities.py
+++ Minecraft/web_utilities.py
@@ -1,8 +1,30 @@
 from git import Repo, GitCommandError
 
+from scrapy import Spider, Item, Field
 
 def clone_repo(url, target):
     try:
         Repo.clone_from(url, target)
     except GitCommandError:
         print("Repository already exists, skipping clone")
+
+# Going to wait on this until I get some sort of ok from Curse...
+class Project(Item):
+    url = Field()
+    name = Field()
+
+class CurseforgeSpider(Spider):
+    home_url = r'http://minecraft.curseforge.com/mc-mods'
+
+    def parse(self, response):
+        for index in range(1, 16):
+
+        return [Project(name=e.extract()) for e in response.css("h2 a::text")]
+
+
+def curseforge_mod_scrape(page):
+
+    # for
+    pass
+    # li.project-list-item:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)
+    # li.project-list-item:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)
